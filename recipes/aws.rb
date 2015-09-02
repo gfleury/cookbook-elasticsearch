9449166f299dd90774774d8d460da206788062d3
gfleury <gfleury@gmail.com>
2015-09-01 21:24:46 -0300

    fix

--- recipes/aws.rb
+++ recipes/aws.rb
@@ -1,3 +1,9 @@
 node.default[:elasticsearch][:plugin][:mandatory] = Array(node[:elasticsearch][:plugin][:mandatory] | ['cloud-aws'])
 
+service "elasticsearch" do
+   supports :status => true, :restart => true
+   action [ :enable ]
+end
+
+
 install_plugin "elasticsearch/elasticsearch-cloud-aws/#{node.elasticsearch['plugins']['elasticsearch/elasticsearch-cloud-aws']['version']}"
